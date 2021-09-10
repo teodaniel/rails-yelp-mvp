ac74e405d2bda4f3a9717ab1e8ac9cfc58251701
Daniel Teo <teodaniel14@gmail.com>
2021-09-10 08:58:31 +0800

    reviews routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/show'
+  get 'reviews/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:show, :new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+5.times do
+  restaurant_attr = { name: Faker::Restaurant, address: Faker::Address.full_address }
+  restaurant = Restaurant.create!(restaurant_attr)
+  puts "Created #{restaurant.name}"
+  rating_attr = { content: Faker::Restaurant.review, restaurant_id: restaurant.id }
+  review = Rating.create!(rating_attr)
+  puts "Created a rating of #{review.rating} for #{restaurant.name}"
+end
+
+puts 'Finished!'
